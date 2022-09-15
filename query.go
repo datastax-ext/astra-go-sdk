8f8470b0ca5eb8dc0dd41e52f274f0e77b1c62cf
Spencer Connaughton <spencer@spencerconnaughton.com>
2022-09-15 09:24:10 -0400

    Add batch queries (#28)
    Add batch queries.

--- query.go
+++ query.go
@@ -7,6 +7,16 @@ import (
 	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
+type BatchType uint8
+
+// Batch types for BatchQuery.
+// See https://docs.datastax.com/en/cql-oss/3.x/cql/cql_reference/cqlBatch.html
+const (
+	BatchLogged BatchType = iota
+	BatchUnlogged
+	BatchCounter
+)
+
 type params struct {
 	keyspace string
 }
@@ -16,6 +26,17 @@ func (p *params) toQueryParamsProto() *pb.QueryParameters {
 		return nil
 	}
 	res := &pb.QueryParameters{}
+	if p.keyspace != "" {
+		res.Keyspace = &wrapperspb.StringValue{Value: p.keyspace}
+	}
+	return res
+}
+
+func (p *params) toBatchParamsProto() *pb.BatchParameters {
+	if p == nil {
+		return nil
+	}
+	res := &pb.BatchParameters{}
 	if p.keyspace != "" {
 		res.Keyspace = &wrapperspb.StringValue{Value: p.keyspace}
 	}
@@ -32,11 +53,9 @@ func (p *queryParams) createIfEmpty() {
 	}
 }
 
-// Keyspace sets the keyspace to use for the query.
-func (p *queryParams) Keyspace(value string) *queryParams {
+func (p *queryParams) keyspace(value string) {
 	p.createIfEmpty()
 	p.params.keyspace = value
-	return p
 }
 
 // Query is a configurable and executable Stargate query. Use Client.Query to
@@ -46,6 +65,18 @@ type Query struct {
 	cql    string
 	values []any
 	queryParams
+}
+
+// Keyspace sets the keyspace to use for the query.
+func (q *Query) Keyspace(value string) *Query {
+	q.queryParams.keyspace(value)
+	return q
+}
+
+// Exec executes the Query using the client that created it and returns the
+// resultant rows.
+func (q *Query) Exec() (Rows, error) {
+	return q.client.execQuery(q)
 }
 
 func (q *Query) toQueryProto() (*pb.Query, error) {
@@ -61,11 +92,65 @@ func (q *Query) toQueryProto() (*pb.Query, error) {
 }
 
 func (q *Query) toBatchQueryProto() (*pb.BatchQuery, error) {
-	return &pb.BatchQuery{}, nil
+	vs, err := valuesToProto(q.values)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert values to proto: %v", err)
+	}
+	return &pb.BatchQuery{
+		Cql:    q.cql,
+		Values: &pb.Values{Values: vs},
+	}, nil
 }
 
-// Exec executes the Query using the client that created it and returns the
-// resultant rows.
-func (q *Query) Exec() (Rows, error) {
-	return q.client.execQuery(q)
+// BatchQuery is a configurable and executable Stargate batch query. Use
+// Client.Batch to create a BatchQuery.
+type BatchQuery struct {
+	client    *Client
+	batchType BatchType
+	queries   []*Query
+	queryParams
+}
+
+// Keyspace sets the keyspace to use for the batch query.
+func (b *BatchQuery) Keyspace(value string) *BatchQuery {
+	b.queryParams.keyspace(value)
+	return b
+}
+
+func (b *BatchQuery) toProto() (*pb.Batch, error) {
+	qs := make([]*pb.BatchQuery, len(b.queries))
+	for i, q := range b.queries {
+		bq, err := q.toBatchQueryProto()
+		if err != nil {
+			return nil, err
+		}
+		qs[i] = bq
+	}
+
+	var t pb.Batch_Type
+	switch b.batchType {
+	case BatchLogged:
+		t = pb.Batch_LOGGED
+	case BatchUnlogged:
+		t = pb.Batch_UNLOGGED
+	case BatchCounter:
+		t = pb.Batch_COUNTER
+	default:
+		return nil, fmt.Errorf("unknown batch type: %v", b.batchType)
+	}
+
+	res := &pb.Batch{
+		Queries:    qs,
+		Parameters: b.params.toBatchParamsProto(),
+	}
+	if t != pb.Batch_LOGGED {
+		res.Type = t
+	}
+
+	return res, nil
+}
+
+// Exec executes the BatchQuery using the client that created it.
+func (b *BatchQuery) Exec() error {
+	return b.client.execBatch(b)
 }
